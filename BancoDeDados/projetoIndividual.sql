24cee74f4cd069ccd5709589b965be5be06fce83
JoaoRossii <joao.rossi@sptech.school>
2024-06-04 10:45:25 -0300

    Inserção de dados no banco de dados de outra maquina

--- BancoDeDados/projetoIndividual.sql
+++ BancoDeDados/projetoIndividual.sql
@@ -10,8 +10,20 @@ create table usuario (
     vaporwave varchar(3)
 );
 
+create table quiz(
+	idQuiz int primary key auto_increment,
+    certas int,
+    erradas int,
+    fkUsuario int,
+    foreign key (fkUsuario) references usuario(ra)
+);
+
 alter table usuario add constraint checkUsuario check (vaporwave in ('Sim', 'Não'));
 
 select * from usuario;
+
+select * from quiz;
+
+select sum(certas) as 'Acertos', sum(erradas) as 'Erros' from quiz;
 
 truncate table usuario;
